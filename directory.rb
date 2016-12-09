5c0eb3ceb4514bc72c6d2d951c26ad736836686f
Pedro Castanheira <josepedrocastanheira@gmail.com>
2016-12-09 11:35:56 +0000

    Saving the data to a file

--- directory.rb
+++ directory.rb
@@ -1,24 +1,5 @@
 @students = []
 
-def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "9. Exit" # 9 because we'll be adding more items
-end
-
-def process(selection)
-  case selection
-    when "1"
-      input_students
-    when "2"
-      show_students
-    when "9"
-      exit
-    else
-      puts "I don't know what you mean, try again"
-  end
-end
-
 def interactive_menu
   loop do
     print_menu
@@ -26,10 +7,26 @@ def interactive_menu
   end
 end
 
-def show_students
-  print_header
-  print_students_list
-  print_footer
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "9. Exit" # 9 because we'll be adding more items
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "3"
+    save_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you mean, try again"
+  end
 end
 
 def input_students
@@ -46,6 +43,24 @@ def input_students
     # get another name from the user
     name = gets.chomp
   end
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 def print_header
